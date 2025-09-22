aa9c1d43019f84adfeb87ed689d7e88ee7d24015
nawnitk2006 <nawnitk2006@gmail.com>
2025-09-22 23:55:14 +0530

    Arrays

---
+++ Arrays/input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={3,5,1,9,3};
+  
+    cout<< sizeof(arr)<<endl;;
+    for(int i=0; i<5;i++){
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+}
